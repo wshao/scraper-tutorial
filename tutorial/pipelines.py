093fb2c2b936697516c37c0684944bd3dcd15040
Wei Shao <weishao@outlook.com>
2017-03-08 23:56:15 +0000

    update pipeline

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 
 # Define your item pipelines here
 #
@@ -7,5 +9,24 @@
 
 
 class TutorialPipeline(object):
+    # def process_item(self, item, spider):
+    #     return item
+
+    def open_spider(self, spider):
+        self.file = open('new_items.json', 'wb')
+        self.file.write("[\n")
+
+    def close_spider(self, spider):
+        self.file.write("\n]")
+        self.file.close()
+
     def process_item(self, item, spider):
+        # page = item["page"]
+        # self.log("pipeline page: %s" % page)
+        records = item.get("records", [])
+
+        for record in records:
+            # self.log("pipeline record: %s" % record)
+            line = json.dumps(dict(record)) + ",\n"
+            self.file.write(line)
         return item
